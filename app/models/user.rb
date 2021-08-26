b74ad4f34ab71e9e6011d9a9944bcd88efc32e92
AndyRama <andyramaroson@gmail.com>
2021-08-26 18:44:05 +0200

    campaign works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
+require "subscribe_to_newsletter_service.rb"
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :subscribe_to_newsletter
+
+  private
+
+  def subscribe_to_newsletter
+    SubscribeToNewsletterService.new(self).call
+  end
 end
+
